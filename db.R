d344c8842918cacfe8ff786d37178571fafcc700
Colin Millar <colinpmillar@gmail.com>
2017-08-31 21:14:25 +0200

    changes to fit play nice with TAF webpage

--- db.R
+++ db.R
@@ -1,17 +1,15 @@
 ## Download and preprocess data, write TAF input tables
 
-## Before: catch.csv, surveys_all.csv (ftp)
-## After:  catch.csv, survey.csv, summary.csv (db)
+## Before: catch.csv, surveys_all.csv (initial-data) - pre uploaded
+## After:  catch.csv, survey.csv, summary.csv (input)
 
 library(icesTAF)
-
-ftp <- "https://raw.githubusercontent.com/ices-taf/ftp/master/wgef/2015/rjm-347d/"
 
 mkdir("db")
 
 ## Download data, select years and surveys of interest
-catch <- read.taf(paste0(ftp, "db/catch.csv"))
-survey <- read.taf(paste0(ftp, "db/surveys_all.csv"))
+catch <- read.taf("initial-data/catch.csv"))
+survey <- read.taf("initial-data/surveys_all.csv")
 survey <- survey[survey$Year %in% 1993:2014, names(survey) != "Unknown"]
 
 ## Scale each survey to average 1, combine index as average of three surveys
