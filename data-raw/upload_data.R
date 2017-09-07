bc07b9dd802163e81eb6f7b0cc394fe33466abe0
Arni Magnusson <arnima@hafro.is>
2017-09-07 18:30:22 +0200

    Shorten function name and args

--- data-raw/upload_data.R
+++ data-raw/upload_data.R
@@ -3,11 +3,12 @@
 ## Before: catch.csv, surveys_all.csv (user dir)
 ## After:  catch.csv, surveys_all.csv (TAF database)
 
-upload.dataraw.taf <- function(filename, analysisName) {
-  file <- httr::upload_file(filename)
-  url <- sprintf("http://localhost:1234/fs/%s/data-raw/%s", analysisName, basename(file$path))
-  httr::POST(url, body = file)
+upload <- function(file, analysis)
+{
+  file <- httr::upload_file(file)
+  url <- sprintf("http://localhost:1234/fs/%s/data-raw/%s", analysis, basename(file$path))
+  httr::POST(url, body=file)
 }
 
-upload.dataraw.taf("d:/projects/ices-taf/ftp/wgef/2015/rjm-347d/db/catch.csv", "2015_rjm-347d")
-upload.dataraw.taf("d:/projects/ices-taf/ftp/wgef/2015/rjm-347d/db/surveys_all.csv", "2015_rjm-347d")
+upload("d:/projects/ices-taf/ftp/wgef/2015/rjm-347d/db/catch.csv", "2015_rjm-347d")
+upload("d:/projects/ices-taf/ftp/wgef/2015/rjm-347d/db/surveys_all.csv", "2015_rjm-347d")
