8fb3cc13af9ac4d942a95cd5a8c56a378dd82922
Arni Magnusson <arnima@hafro.is>
2017-09-07 21:58:52 +0200

    Remove upload() definition, now part of icesTAF package

--- data-raw/upload.R
+++ data-raw/upload.R
@@ -3,13 +3,7 @@
 ## Before: catch.csv, surveys_all.csv (user dir)
 ## After:  catch.csv, surveys_all.csv (TAF database)
 
-upload <- function(analysis, file)
-{
-  file <- httr::upload_file(file)
-  url <- sprintf("http://localhost:1234/fs/%s/data-raw/%s",
-                 analysis, basename(file$path))
-  httr::POST(url, body=file)
-}
+library(icesTAF)
 
 owd <- setwd("d:/projects/ices-taf/ftp/wgef/2015/rjm-347d/db")
 upload("2015_rjm-347d", "catch.csv")
