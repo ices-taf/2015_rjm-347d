1662c2a4d32ec3fce81536e39bea67f8d769d2ff
Arni Magnusson <arnima@hafro.is>
2017-09-07 18:32:27 +0200

    Define user directory in one place, format in 80 cols

--- data-raw/upload_data.R
+++ data-raw/upload_data.R
@@ -6,9 +6,12 @@
 upload <- function(file, analysis)
 {
   file <- httr::upload_file(file)
-  url <- sprintf("http://localhost:1234/fs/%s/data-raw/%s", analysis, basename(file$path))
+  url <- sprintf("http://localhost:1234/fs/%s/data-raw/%s",
+                 analysis, basename(file$path))
   httr::POST(url, body=file)
 }
 
-upload("d:/projects/ices-taf/ftp/wgef/2015/rjm-347d/db/catch.csv", "2015_rjm-347d")
-upload("d:/projects/ices-taf/ftp/wgef/2015/rjm-347d/db/surveys_all.csv", "2015_rjm-347d")
+owd <- setwd("d:/projects/ices-taf/ftp/wgef/2015/rjm-347d/db")
+upload("catch.csv", "2015_rjm-347d")
+upload("surveys_all.csv", "2015_rjm-347d")
+setwd(owd)
