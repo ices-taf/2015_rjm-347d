de1e6de0eefd68a89c295aabadf640b044218781
Arni Magnusson <arnima@hafro.is>
2017-09-08 14:36:13 +0200

    Whitespace

--- model.R
+++ model.R
@@ -12,7 +12,7 @@ mkdir("model")
 load("input/input.RData")
 
 ## Apply DLS method 3.2
-i1 <- survey$Index[nrow(survey)-(6:2)] # five year period from n-6 to n-2
+i1 <- survey$Index[nrow(survey)-(6:2)]  # five year period from n-6 to n-2
 dls <- DLS3.2(mean(catch$Catch), survey$Index, i1=i1)
 
 write.dls(dls, "model/dls.txt")
