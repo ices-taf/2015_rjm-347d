4ceade3fbe3cde9eec565ec210ed0dfe452555aa
Arni Magnusson <arnima@hafro.is>
2017-09-13 15:21:56 +0200

    Upload with "raw" option

--- _raw/upload.R
+++ _raw/upload.R
@@ -6,6 +6,6 @@
 library(icesTAF)
 
 owd <- setwd("d:/projects/ices-taf/ftp/wgef/2015/rjm-347d/raw")
-upload("2015_rjm-347d", "catch.csv")
-upload("2015_rjm-347d", "surveys_all.csv")
+upload("2015_rjm-347d", "raw", "catch.csv")
+upload("2015_rjm-347d", "raw", "surveys_all.csv")
 setwd(owd)
