f7bce6c0c7adefacb895da3545ace2befb3c2a2d
Arni Magnusson <arnima@hafro.is>
2017-10-03 22:36:28 +0200

    Use tafpng(), lim(), ido()

--- report.R
+++ report.R
@@ -12,13 +12,13 @@ survey <- read.taf("data/survey.csv")
 dls <- read.dls("output/dls.txt")
 
 ## Plot
-png("report/dls.png", width=800, height=600, pointsize=24)
+tafpng("dls")
 xlim <- c(1990, 2016)
-ylim <- c(0, max(pretty(survey$Index)))
-plot(Index~Year, survey, type="b", lty=3, xlim=xlim, ylim=ylim, yaxs="i", las=1)
+plot(Index~Year, survey, type="b", lty=3, xlim=xlim, ylim=lim(survey$Index),
+     yaxs="i", las=1)
 segments(2008, dls$i1bar, 2012, lwd=2)
 segments(2013, dls$i2bar, 2014, lwd=2)
 title(main="rjm-347d (2015 DLS advice)")
 grid()
 box()
-invisible(dev.off())
+ido()
