7d89cfb8850fd15690ecb3bbb23433fef524e2bf
Arni Magnusson <arnima@hafro.is>
2017-10-30 17:52:37 +0100

    Round summary

--- report.R
+++ report.R
@@ -1,4 +1,4 @@
-## Plot data and results
+## Prepare plots and tables for report
 
 ## Before: survey.csv (data), dls.txt (output)
 ## After:  dls.png (report)
@@ -8,12 +8,17 @@ library(icesTAF)
 mkdir("report")
 
 ## Get DLS data and results
-survey <- read.taf("data/survey.csv")
+summary <- read.taf("data/summary.csv")
 dls <- read.dls("output/dls.txt")
 
 ## Plot
 tafpng("dls")
-plot(survey$Year, survey$Index, type="b", lty=3, ylim=lim(survey$Index),
+plot(summary$Year, summary$Index, type="b", lty=3, ylim=lim(summary$Index),
      yaxs="i", main="Survey", xlab="Year", ylab="Index",
      panel.first=grid(lwd=2))
 dev.off()
+
+## Table
+summary <- rnd(summary, "Catch")
+summary <- rnd(summary, "Index", 3)
+write.taf(summary, "report/summary.csv")
