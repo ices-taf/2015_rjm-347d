5a641b5b79f69afc22552cdd9a0ada275f6308cd
Arni Magnusson <arnima@hafro.is>
2017-11-01 06:50:38 +0100

    Write survey.png

--- report.R
+++ report.R
@@ -1,18 +1,16 @@
 ## Prepare plots and tables for report
 
-## Before: survey.csv (data), dls.txt (output)
-## After:  dls.png (report)
+## Before: summary.csv (data)
+## After:  survey.png (report)
 
 library(icesTAF)
 
 mkdir("report")
 
-## Get DLS data and results
 summary <- read.taf("data/summary.csv")
-dls <- read.dls("output/dls.txt")
 
 ## Plot
-tafpng("dls")
+tafpng("survey")
 plot(summary$Year, summary$Index, type="b", lty=3, ylim=lim(summary$Index),
      yaxs="i", main="Survey", xlab="Year", ylab="Index",
      panel.first=grid(lwd=2))
