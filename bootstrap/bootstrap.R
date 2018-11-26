9d0e328807209fbf3f7fbdc3d754d246ef8bd720
Arni Magnusson <arnima@hafro.is>
2018-11-26 18:34:49 +0100

    Handle different source types

--- bootstrap/bootstrap.R
+++ bootstrap/bootstrap.R
@@ -16,10 +16,24 @@ for(dat in datasets)
 software <- bibtex::read.bib("SOFTWARE.bib")
 for(soft in software)
 {
-  spec <- remotes::parse_repo_spec(soft$source)
-  url <- paste0("https://api.github.com/repos/",
-                spec$username, "/", spec$repo, "/tarball/", spec$ref)
-  targz <- paste0(spec$repo, "_", spec$ref, ".tar.gz")
-  suppressWarnings(download(url, destfile=file.path("software", targz)))
-  remotes::install_github(soft$source, upgrade=FALSE, force=TRUE)
+  if(grepl("@", soft$source))
+  {
+    spec <- remotes::parse_repo_spec(soft$source)
+    url <- paste0("https://api.github.com/repos/",
+                  spec$username, "/", spec$repo, "/tarball/", spec$ref)
+    targz <- paste0(spec$repo, "_", spec$ref, ".tar.gz")
+    suppressWarnings(download(url, destfile=file.path("software", targz)))
+    remotes::install_github(soft$source, upgrade=FALSE, force=TRUE)
+  }
+  else if(grepl("^http", soft$source))
+  {
+    download(soft$source, dir="software")
+  }
+  else
+  {
+    cp(soft$source, "software")
+  }
 }
+
+## Remove empty folders
+sapply(c("data","library","software"), rmdir)
