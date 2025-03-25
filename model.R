d0665c8e4f4ef0ae8622d4e5240e3593d0afb1da
Arni Magnusson <thisisarni@gmail.com>
2025-03-25 16:48:28 +1100

    Visual space

--- model.R
+++ model.R
@@ -13,6 +13,6 @@ catch <- read.taf("data/catch.csv")
 survey <- read.taf("data/survey.csv")
 
 # Apply DLS method 3.2, comparing 5 years and 2 years
-dls <- DLS3.2(mean(catch$Catch), survey$Index, len=c(5,2))
+dls <- DLS3.2(mean(catch$Catch), survey$Index, len=c(5, 2))
 
 write.dls(dls, "model/dls.txt")
