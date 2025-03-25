6fc9d645b885e1b523c821427d2f2f0b0140edae
Arni Magnusson <thisisarni@gmail.com>
2025-03-25 16:02:32 +1100

    Visual space

--- data.R
+++ data.R
@@ -13,7 +13,7 @@ survey <- read.taf("boot/data/surveys_all.csv")
 survey <- survey[survey$Year %in% 1993:2014, names(survey) != "Unknown"]
 
 # Scale each survey to average 1, combine index as average of three surveys
-survey[-1] <- sapply(survey[-1], function(x) x/mean(x, na.rm=TRUE))
+survey[-1] <- sapply(survey[-1], function(x) x / mean(x, na.rm=TRUE))
 survey$Index <- rowMeans(survey[-1])
 
 # Finalize tables
